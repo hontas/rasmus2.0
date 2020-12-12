71cde0789df64ec05c2018fa0b82388a982238b9
hontas <lundin.pontus@gmail.com>
2020-12-12 01:44:22 +0100

    deploy

---
+++ precache-manifest.01a7b55e57f919b2a6646757336be386.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c10bdbb541b1a0feeca",
+    "url": "/rasmus2.0/css/app.4fd4dce2.css"
+  },
+  {
+    "revision": "ee58639effcc9558d63d",
+    "url": "/rasmus2.0/css/chunk-8cd433a8.6a6a4e2b.css"
+  },
+  {
+    "revision": "0ba77c2ea40d26c2944c",
+    "url": "/rasmus2.0/css/chunk-f72d3250.da2b0ca2.css"
+  },
+  {
+    "revision": "c09cde5a568b9e9ad750f8689bbbee37",
+    "url": "/rasmus2.0/index.html"
+  },
+  {
+    "revision": "5c10bdbb541b1a0feeca",
+    "url": "/rasmus2.0/js/app.e67bf577.js"
+  },
+  {
+    "revision": "ee58639effcc9558d63d",
+    "url": "/rasmus2.0/js/chunk-8cd433a8.50366329.js"
+  },
+  {
+    "revision": "0ba77c2ea40d26c2944c",
+    "url": "/rasmus2.0/js/chunk-f72d3250.c4775342.js"
+  },
+  {
+    "revision": "72511e85eddc494bb314",
+    "url": "/rasmus2.0/js/chunk-vendors.5fad5353.js"
+  },
+  {
+    "revision": "4360c8b7e3f390c55a92758f7e818495",
+    "url": "/rasmus2.0/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/rasmus2.0/robots.txt"
+  }
+]);
